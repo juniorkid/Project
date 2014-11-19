094be6b42e9428391c1aa6dd6536e767e9cdb1ce
kakcare <vicharatti@hotmail.com>
2014-11-19 17:41:04 +0700

    test homepage

--- webpageDesign/index.js
+++ webpageDesign/index.js
@@ -3,6 +3,15 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/member',function(req,res){
+	var members = [
+		{name: 'Dream', id: '5510110132 ',sex: 'male'},
+		{name: 'Care', id: '5510110049 ',sex: 'male'},
+		{name: 'Ne', id: '5510110438 ',sex: 'male'},
+		{name: 'Beer', id: '5510110230 ',sex: 'female'}
+	];
+	res.send(members);
+})
 var server = app.listen(3000, function () {
 
   	console.log("server is running")
