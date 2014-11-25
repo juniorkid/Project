de2c710bff2c85120b3463be8d834167ab9be6da
kakcare <vicharatti@hotmail.com>
2014-11-25 19:37:16 +0700

    Add realtime system by socket.io tools

--- webpageDesign/index.js
+++ webpageDesign/index.js
@@ -1,26 +1,32 @@
 var express = require('express')
 var mongojs = require('mongojs');
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var db = mongojs('Project', ['person']);
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
 
-
-app.get('/api/books',function(req,res){
+app.get('/api/book',function(req,res){
 	db.person.find({},function(err,persons){
 		res.send(persons);
-	})	
+	});
 })
 
-app.post('/api/books',function(req,res){
+app.post('/api/book',function(req,res){
 	db.person.insert((req.body),function(err,persons){
 		res.send(persons);	
+		io.emit("person:refresh");
 	});
 });
 
-var server = app.listen(3000, function () {
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-  	console.log("server is running")
-
+http.listen(8080, function () {
+	console.log("server is running")
 })
--- webpageDesign/public/js/app.js
+++ webpageDesign/public/js/app.js
@@ -1,9 +1,29 @@
-angular.module("myApp", [])
-.controller('mainCtrl', function($scope, $http){
+angular.module("myApp", ['btford.socket-io'])
+.factory('socketIO', function(socketFactory){
+	 return socketFactory({
+    	ioSocket: io.connect("http://localhost:8080")
+    });
+})
+.controller('mainCtrl', function($scope, $http, socketIO){
 	$scope.persons = [];
+	$scope.personInstance = {};
+	refreshPersons();
 
-	$http.get('/api/books').success(function(data){
-		$scope.persons = data;
-	})
+	function refreshPersons(){
+		$http.get('/api/book').success(function(data){
+			$scope.persons = data;
+		})
+	}
+
+	$scope.save = function(){
+		$http.post('/api/book', $scope.personInstance).success(function(data){
+			$scope.persons.push(data);
+			$scope.personInstance = {};
+		});
+	}
+
+	socketIO.on('person:refresh', function(){
+		refreshPersons();
+	});
 
 })
